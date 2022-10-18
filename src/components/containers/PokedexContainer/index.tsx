93d11f86c2629a6180da914897aff01ab703599d
Yuriy <nikorezar@gmail.com>
2022-10-18 22:54:41 +0300

    feat: add list of pokemon

--- src/components/containers/PokedexContainer/index.tsx
+++ src/components/containers/PokedexContainer/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 
 import { useRequestPokemonQueries } from '@utils/api/hooks';
+
 import styles from './pokedex.module.css';
 
 const PokedexContainer = () => {
-  const [offset, setOffset] = useState(20);
+  const [offset, setOffset] = useState(10);
   const results = useRequestPokemonQueries({ offset });
 
   const isFetching = results.some((result) => result.isFetching);
@@ -18,9 +19,14 @@ const PokedexContainer = () => {
       <div className={styles.pokedex_container}>
         <div>card</div>
         <ul className={styles.list_container}>
-          {pokemons.map(({ sprites }) => (
-            <li>
-              <img src={sprites.front_default ?? ''} alt='pokemon li' />
+          {pokemons.map(({ sprites, name }) => (
+            <li key={name} className={styles.pokemon_item}>
+              <img
+                className={styles.pokemon_item_image}
+                src={sprites.front_default ?? ''}
+                alt='pokemon li'
+              />
+              {name}
             </li>
           ))}
         </ul>
